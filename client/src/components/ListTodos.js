5eea256887c4ec21a03528c59efc7bb60f2b4230
Neno Zidic <zidicneno@gmail.com>
2020-12-13 22:07:10 +0100

    Init commit

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+import EditTodo from './EditTodo';
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  // delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json(); // vraca array nasih objekata
+      setTodos(jsonData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos); // takoder vraca array nasih objekata
+  return (
+    <Fragment>
+      {''}
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <tr>{todo.description}</tr>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
